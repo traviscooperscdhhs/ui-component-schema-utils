3c913006e113694869b08b1bf2060fe6a70e4afa
Michael Allen <michael.allen@scdhhs.gov>
2015-07-22 16:23:26 -0400

    Add jscs, update build + test scripts

--- src/ui-component-schema-utils.js
+++ src/ui-component-schema-utils.js
@@ -7,7 +7,7 @@ import Immutable from 'immutable';
  * not exist, but we dont want the result to be "undefined" as that would
  * effectively "delete" the value.
  */
-function maybe(value){
+function maybe(value) {
   return value || null;
 }
 
@@ -24,14 +24,14 @@ class SchemaUtils {
    * @param {object} componentId
    * @return {{id: *, previous: *, parent: *, next: *}}
    */
-  static getMetaData(schema, componentId){
+  static getMetaData(schema, componentId) {
     let exists = _.has(schema.components, componentId);
     return {
       id: componentId,
-      previous: _.findKey(schema.components, { next: componentId }),
-      parent: _.findKey(schema.components, { child: componentId }),
-      next: exists? schema.components[componentId].next : null,
-      child: exists? schema.components[componentId].child : null
+      previous: _.findKey(schema.components, {next: componentId}),
+      parent: _.findKey(schema.components, {child: componentId}),
+      next: exists ? schema.components[componentId].next : null,
+      child: exists ? schema.components[componentId].child : null
     };
   }
 
@@ -43,11 +43,12 @@ class SchemaUtils {
    * @param {string} componentId
    * @returns {object} updated schema
    */
-  static getRootId(schema, componentId){
+  static getRootId(schema, componentId) {
     let meta = SchemaUtils.getMetaData(schema, componentId);
-    while(meta && !meta.parent){
+    while(meta && !meta.parent) {
       meta = SchemaUtils.getMetaData(schema, meta.previous);
     }
+
     return meta.parent;
   }
 
@@ -58,18 +59,19 @@ class SchemaUtils {
    * @param {string} componentId
    * @returns {object}
    */
-  static moveUp(schema, componentId){
+  static moveUp(schema, componentId) {
     let meta = SchemaUtils.getMetaData(schema, componentId);
-    let updates = { components: {} };
-    if ( meta.previous ) {
-      let previousPreviousNext = _.findKey(schema.components, { next: meta.previous });
-      let previousPreviousChild = _.findKey(schema.components, { child: meta.previous });
+    let updates = {components: {}};
+    if (meta.previous) {
+      let previousPreviousNext = _.findKey(schema.components, {next: meta.previous});
+      let previousPreviousChild = _.findKey(schema.components, {child: meta.previous});
+
       // 1. Parent previous 'next' needs to point to ME
-      if ( previousPreviousNext ) {
+      if (previousPreviousNext) {
         updates.components[previousPreviousNext] = {
           next: meta.id
         };
-      } else if ( previousPreviousChild ) {
+      } else if (previousPreviousChild) {
         updates.components[previousPreviousChild] = {
           child: meta.id
         };
@@ -78,15 +80,18 @@ class SchemaUtils {
         // so set workflow child to me.
         updates.child = meta.id;
       }
+
       // 2. My next points to previous
       updates.components[meta.id] = {
         next: meta.previous
       };
+
       // 3. Previous next points to my next
       updates.components[meta.previous] = {
         next: maybe(meta.next)
       };
     }
+
     return updates;
   }
 
@@ -97,33 +102,37 @@ class SchemaUtils {
    * @param {string} componentId
    * @returns {object}
    */
-  static moveDown(schema, componentId){
+  static moveDown(schema, componentId) {
     let meta = SchemaUtils.getMetaData(schema, componentId);
-    let updates = { components: {} };
-    if ( meta.next ) {
+    let updates = {components: {}};
+    if (meta.next) {
       let nextNext = schema.components[meta.next].next;
+
       // 1. my previous 'next' points to my next
-      if ( meta.previous ) {
+      if (meta.previous) {
         updates.components[meta.previous] = {
           next: meta.next
         };
-      } else if ( meta.parent ) {
+      } else if (meta.parent) {
         updates.components[meta.parent] = {
           child: meta.next
         };
       } else {
         // If I'm the first page, update the workflow child to point to my next.
-        updates.child =  meta.next;
+        updates.child = meta.next;
       }
+
       // 2. myNext 'next' points to me
       updates.components[meta.next] = {
         next: meta.id
       };
+
       // 3. ME 'next' points to myNext 'next'
       updates.components[meta.id] = {
         next: maybe(nextNext)
       };
     }
+
     return updates;
   }
 
@@ -134,12 +143,12 @@ class SchemaUtils {
    * @param componentId
    * @returns {object} Updated Schema
    */
-  static nest(schema, componentId){
+  static nest(schema, componentId) {
     let meta = SchemaUtils.getMetaData(schema, componentId);
-    let updates = { components: {} };
-    if ( meta.previous ) {
+    let updates = {components: {}};
+    if (meta.previous) {
       let previousChild = schema.components[meta.previous].child;
-      if ( previousChild ) {
+      if (previousChild) {
         let parentLastChild = SchemaUtils.getLastSiblingId(schema, previousChild);
         updates.components[meta.previous] = {
           next: maybe(meta.next)
@@ -153,10 +162,12 @@ class SchemaUtils {
           child: meta.id
         };
       }
+
       updates.components[meta.id] = {
         next: null
       };
     }
+
     return updates;
   }
 
@@ -167,21 +178,21 @@ class SchemaUtils {
    * @param {string} componentId
    * @returns {object} Updated Schema
    */
-  static unNest(schema, componentId){
+  static unNest(schema, componentId) {
     let meta = SchemaUtils.getMetaData(schema, componentId);
-    let updates = { components: {} };
+    let updates = {components: {}};
     let parent = meta.parent;
     let previous = meta.previous;
 
-    if ( parent ) {
+    if (parent) {
       updates.components[parent] = {
-        child: meta.next ,
-        next:  meta.id
+        child: meta.next,
+        next: meta.id
       };
     } else {
       parent = SchemaUtils.getRootId(schema, previous);
       updates.components[parent] = {
-        next:  meta.id
+        next: meta.id
       };
       updates.components[previous] = {
         next: meta.next
@@ -202,13 +213,14 @@ class SchemaUtils {
    * @param {string} componentId
    * @returns {object}
    */
-  static getLastSiblingId(schema, componentId){
+  static getLastSiblingId(schema, componentId) {
     let head = schema.components[componentId];
     let id = componentId;
-    while(head.next){
+    while(head.next) {
       id = head.next;
       head = schema.components[id];
     }
+
     return id;
   }
 
@@ -223,14 +235,15 @@ class SchemaUtils {
    * @param {string} componentId
    * @param {function} fn
    */
-  static traverse(schema, componentId, fn){
+  static traverse(schema, componentId, fn) {
     let head = schema.components[componentId];
     let id = componentId;
-    while(head){
+    while(head) {
       fn(id, head);
-      if ( head.child ) {
+      if (head.child) {
         SchemaUtils.traverse(schema, head.child, fn);
       }
+
       id = head.next;
       head = schema.components[id];
     }
@@ -244,30 +257,33 @@ class SchemaUtils {
    * @param {string} componentId
    * @returns {object}
    */
-  static removeComponent(schema, componentId){
+  static removeComponent(schema, componentId) {
     let meta = SchemaUtils.getMetaData(schema, componentId);
-    let updates = { components: {} };
+    let updates = {components: {}};
+
     // nullify the passed in componentId
     updates.components[componentId] = null;
+
     // take the component that points to me and make it
     // point to my next sibling instead
-    if ( meta.previous ) {
+    if (meta.previous) {
       updates.components[meta.previous] = {
         next: maybe(meta.next)
       };
-    } else if ( meta.parent ) {
+    } else if (meta.parent) {
       // if I am a direct child, point my parent to my next sibling
       updates.components[meta.parent] = {
         child: maybe(meta.next)
       };
-    } else if ( schema.child === componentId ) {
+    } else if (schema.child === componentId) {
       // if I am the direct child of the parent schema, point to
       // my next sibling
       updates.child = maybe(meta.next);
     }
+
     // remove any child components
-    if ( meta.child ) {
-      SchemaUtils.traverse(schema, meta.child, function(id, components){
+    if (meta.child) {
+      SchemaUtils.traverse(schema, meta.child, function(id, components) {
         updates.components[id] = null;
       });
     }
@@ -283,24 +299,25 @@ class SchemaUtils {
    * @param {object} childSchema - new component to add
    * @returns {object}
    */
-  static addNewChildComponent(schema, parentId, childSchema){
+  static addNewChildComponent(schema, parentId, childSchema) {
     let newComponentId = _.camelCase([childSchema.config.name, childSchema.type].join(' '));
-    let parent = parentId? schema.components[parentId] : schema;
+    let parent = parentId ? schema.components[parentId] : schema;
     let updates = {
       components: {
         [newComponentId]: Immutable.fromJS(childSchema).setIn(['config', 'id'], newComponentId).toJSON()
       }
     };
+
     // if the parent already has children, set the new component as the last child
     // NOTE: parent could be a root schema with children
-    if ( parent.child ) {
+    if (parent.child) {
       let lastSibling = SchemaUtils.getLastSiblingId(schema, parent.child);
       updates.components[lastSibling] = {
         next: newComponentId
       };
-    } else if ( parentId ){
+    } else if (parentId) {
       // parent is a new parent without existing children
-      updates.components[parentId] = { child:  newComponentId };
+      updates.components[parentId] = {child: newComponentId};
     } else {
       // assume no parentId means we are adding a component to root schema
       updates.child = newComponentId;
